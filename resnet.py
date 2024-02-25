e16c7da3a038bc9c93395f8cae396dd5c3bc1f10
Mustafa-MS <mustafamahmood1991@gmail.com>
2024-02-25 11:59:44 +0300

    the resnet code now have a faster datagenerator ut the results are not so great.

--- resnet.py
+++ resnet.py
@@ -1,24 +1,51 @@
 from datetime import datetime
-
 import numpy as np
 import os
 from tensorflow.keras import layers, models, Input
-#import matplotlib.pyplot as plt
-from time import time
 import tensorflow as tf
-#import keras
 from tensorflow.keras import layers
 from tensorflow.keras.utils import Sequence
-from tensorflow.keras.models import Sequential
 import tensorflow_addons as tfa
 import pandas
 from sklearn.utils.class_weight import compute_class_weight
 
-checkpoint_path = "/home/mustafa/project/checkpoints/cp.ckpt"
+checkpoint_path = "/home/mustafa/project/checkpoints/resnet100/{epoch:02d}-{val_loss:.2f}.keras"
 checkpoint_dir = os.path.dirname(checkpoint_path)
 
 nodules_csv = pandas.read_csv("/home/mustafa/project/LUNA16/cropped_nodules.csv")
 base_dir = "/home/mustafa/project/LUNA16/cropped_nodules/"
+nodules = nodules_csv.rename(columns={'SN': 'ID'})
+
+# Split abnormal and normal nodules
+abnormal_nodules = nodules[nodules['state'] == 1]
+normal_nodules = nodules[nodules['state'] == 0]
+
+# Sample for validation
+val_abnormal = abnormal_nodules.sample(frac=0.10)
+print("val_abnormal = ", len(val_abnormal))
+val_normal = normal_nodules.sample(frac=0.10)
+print("val_abnormal = ", len(val_normal))
+
+# Get the train split by dropping the validation samples
+train_abnormal = abnormal_nodules.drop(val_abnormal.index)
+print("train_abnormal = ", len(train_abnormal))
+train_normal = normal_nodules.drop(val_normal.index)
+print("train normal = ", len(train_normal))
+# Convert IDs to paths
+val_data = (val_abnormal['ID'].astype(str) + '.npy').tolist() + (val_normal['ID'].astype(str) + '.npy').tolist()
+train_data = (train_abnormal['ID'].astype(str) + '.npy').tolist() + (train_normal['ID'].astype(str) + '.npy').tolist()
+
+# Get the labels
+val_labels = val_abnormal['state'].tolist() + val_normal['state'].tolist()
+train_labels = train_abnormal['state'].tolist() + train_normal['state'].tolist()
+
+print("x_val = ", len(val_data))
+print("x_train = ", len(train_data))
+print("y_val = ", len(val_labels))
+print("y_train = ", len(train_labels))
+
+
+'''
 all_image_paths = os.listdir(base_dir)
 all_image_paths = sorted(all_image_paths,key=lambda x: int(os.path.splitext(x)[0]))
 print("all imag paths ln= ",len(all_image_paths))
@@ -48,7 +75,7 @@ for num_nodule in val_data:
 print("x_val = ", len(val_data))
 print("x_train = ", len(train_image_paths))
 print("y_val = ", len(val_label))
-
+'''
 # Assuming y_train is your array of labels
 class_weights = compute_class_weight('balanced', classes=[0, 1], y=train_labels)
 
@@ -180,7 +207,7 @@ class DataGenerator(Sequence):
 
 ## Defining and training the model
 
-input_tensor = Input(shape=(31, 31, 31, 1))
+input_tensor = Input(shape=(None, None, None, 1))
 
 # Initial Convolution
 x = layers.Conv3D(32, 3, strides=2, padding="same", kernel_regularizer=tf.keras.regularizers.l2(l2_lambda))(input_tensor)
@@ -208,20 +235,20 @@ model = models.Model(input_tensor, output_tensor)
 
 batch_size=128
 
-train_gen = DataGenerator(train_image_paths, train_labels, base_dir, (31, 31, 31), batch_size=batch_size, shuffle=False)
-test_gen = DataGenerator(val_data, val_label, base_dir, (31, 31, 31), batch_size=batch_size, shuffle=False)
+train_gen = DataGenerator(train_data, train_labels, base_dir, (31, 31, 31), batch_size=batch_size, shuffle=False)
+test_gen = DataGenerator(val_data, val_labels, base_dir, (31, 31, 31), batch_size=batch_size, shuffle=False)
 
 
 #initial_learning_rate = 0.001
 #lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
 #    initial_learning_rate, decay_steps=100000, decay_rate=0.96, staircase=True
 #)
-epochs=50
+epochs=100
 
 initial_learning_rate = 0.1
 final_learning_rate = 0.00001
 learning_rate_decay_factor = (final_learning_rate / initial_learning_rate)**(1/epochs)
-steps_per_epoch = int(len(train_image_paths)/batch_size)
+steps_per_epoch = int(len(train_data)/batch_size)
 
 lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
                 initial_learning_rate=initial_learning_rate,
@@ -248,18 +275,18 @@ cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
 # Create a TensorBoard callback
 log_dir="/home/mustafa/project/LUNA16/"+ datetime.now().strftime("%Y%m%d-%H%M%S")
 tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
-csv_logger = tf.keras.callbacks.CSVLogger('metrics_resnet.csv')
+csv_logger = tf.keras.callbacks.CSVLogger('metrics_resnet100.csv')
 
 #loss=keras.losses.BinaryCrossentropy(from_logits=True)
 model.compile(optimizer=opt,
               loss = tf.keras.losses.BinaryFocalCrossentropy(apply_class_balancing=True, from_logits=True, gamma=5),
               metrics=[BalancedAccuracy(), 'AUC' ,tfa.metrics.F1Score(num_classes=1, threshold=0.5), tf.keras.metrics.SpecificityAtSensitivity(0.5), 'Precision', 'Recall',
                      'TruePositives', 'FalsePositives', 'FalseNegatives', 'TrueNegatives', 'accuracy'])
-model.build(input_shape= (128,None,None,None,1))
+model.build(input_shape= (batch_size,None,None,None,1))
 model.summary()
 # now let's train the model
 #EarlyStop = tf.keras.callbacks.EarlyStopping(monitor='precision', patience=4, restore_best_weights=True,)
 history = model.fit(train_gen, validation_data = test_gen, epochs=epochs, shuffle = False , verbose = 1 , callbacks = [csv_logger, cp_callback],
 use_multiprocessing = True, class_weight=class_weight_dict)
 #,class_weight=None
-model.save("resnet.keras")
+model.save("resnet100.keras")
